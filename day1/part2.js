6fc451765e67dbf8c1c135792c148a866d1fe19c
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2021-12-13 03:16:02 +0000

    remove one map function for part2

--- day1/part2.js
+++ day1/part2.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const sumReducerFn = (previousValue, currentValue) => previousValue + currentValue;
 
 const increasedAccount = fs.readFileSync(`${__dirname}/input.txt`).toString().split('\n')
-.map(depth => parseInt(depth, 10))
-.map((depth, i, depths) => [depth, depths[i + 1], depths[i + 2]])
+.map((depth, i, depths) => [parseInt(depth, 10), parseInt(depths[i + 1], 10), parseInt(depths[i + 2], 10)])
 .filter(slidingWindow => slidingWindow.some(depth => depth !== undefined || depth !== NaN))
 .map(slidingWindow => slidingWindow.reduce(sumReducerFn))
 .map((depth, i, depths) => {
