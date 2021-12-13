5e432d96b2c4a3eb168b711af662cbbb86622cac
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2021-12-13 02:28:21 +0000

    return number of times that increased

--- day1/part1.js
+++ day1/part1.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 let increased = 0;
 
-fs.readFileSync(__dirname + '/input.txt').toString().split('\n').map((depth, index, depths) => {
+fs.readFileSync(`${__dirname}/input.txt`).toString().split('\n').map((depth, index, depths) => {
     if (index > 0 && depth > depths[index - 1]) increased++;
     return depth; 
 });
+
+console.log(increased);
