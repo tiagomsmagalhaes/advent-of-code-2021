a9f6178015b3eef2376b1d64a28fb684e8acbdb0
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2021-12-13 02:40:54 +0000

    cleanup

--- day1/part1.js
+++ day1/part1.js
@@ -1,15 +1,10 @@
 const fs = require('fs');
 
 const increasedAccount = fs.readFileSync(`${__dirname}/input.txt`).toString().split('\n')
-.map((depth, index, depths) => {
-    if (index === 0) {
-        return 0;
-    }
-    if (depth > depths[index - 1]) {
-        return 1;
-    } else {
-        return 0;
-    }
+.map((depth, i, depths) => {
+    if (i === 0) return 0;
+
+    return depth > depths[i - 1] ? 1 : 0;
 })
 .reduce((previousValue, currentValue) => previousValue + currentValue);
 
