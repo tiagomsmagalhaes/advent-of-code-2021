2e94dd482a1914e138d936a56794da8a2ce4bb1c
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2021-12-13 02:37:37 +0000

    refactor with map and reduce

--- day1/part1.js
+++ day1/part1.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
-let increased = 0;
 
-fs.readFileSync(`${__dirname}/input.txt`).toString().split('\n').map((depth, index, depths) => {
-    if (index > 0 && depth > depths[index - 1]) increased++;
-    return depth; 
-});
+const increasedAccount = fs.readFileSync(`${__dirname}/input.txt`).toString().split('\n')
+.map((depth, index, depths) => {
+    if (index === 0) {
+        return 0;
+    }
+    if (depth > depths[index - 1]) {
+        return 1;
+    } else {
+        return 0;
+    }
+})
+.reduce((previousValue, currentValue) => previousValue + currentValue);
 
-console.log(increased);
+console.log(increasedAccount);
