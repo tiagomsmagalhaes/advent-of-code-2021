02b07672e8acddacb44ac35808cc01545b14f0a0
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2021-12-13 03:10:17 +0000

    reuse reducer

--- day1/part2.js
+++ day1/part2.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
+const sumReducerFn = (previousValue, currentValue) => previousValue + currentValue;
 
 const increasedAccount = fs.readFileSync(`${__dirname}/input.txt`).toString().split('\n')
 .map(depth => parseInt(depth, 10))
 .map((depth, i, depths) => [depth, depths[i + 1], depths[i + 2]])
-.filter(slidingWindow => slidingWindow.some(slot => slot !== undefined || slot !== NaN))
-.map(slidingWindow => slidingWindow.reduce((previousValue, currentValue) => previousValue + currentValue))
+.filter(slidingWindow => slidingWindow.some(depth => depth !== undefined || depth !== NaN))
+.map(slidingWindow => slidingWindow.reduce(sumReducerFn))
 .map((depth, i, depths) => {
     if (i === 0) return 0;
 
     return depth > depths[i - 1] ? 1 : 0;
 })
-.reduce((previousValue, currentValue) => previousValue + currentValue);
+.reduce(sumReducerFn);
 
 console.log(increasedAccount);
