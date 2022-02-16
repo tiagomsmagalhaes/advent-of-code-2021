b1d8639431af9c4f9b26d084ffa20b7a1916a0f6
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2022-02-16 16:38:59 +0000

    try 2

--- day3/part2.js
+++ day3/part2.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
-const sum = fs.readFileSync(`${__dirname}/input.txt`)
+let maxDigits = 0;
+let sum = fs.readFileSync(`${__dirname}/input.txt`)
 .toString()
 .split('\n')
-.map(c => c.split('').map(c => parseInt(c, 10)))
-.reduce((prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i])), []);
+.map(c => {
+    const b = c.split('').map(c => parseInt(c, 10))
+    maxDigits = b.length > maxDigits ? b.length : maxDigits;
+    return b;
+})
+
+
+const transposedSum = sum.reduce((prev, next) => next.map((item, i) => (prev[i] || []).concat(next[i])), []);
 
 const gamma = parseInt(sum.map(b => {
     const ones = b.filter(bit => bit === 1).length;
@@ -20,6 +27,35 @@ const epsilon = parseInt(sum.map(b => {
     return ones > zeros ? 0 : 1;
 }).join(''), 2);
 
-const lifeSupportRating = oxygen * epsilon;
+const powerConsumption = gamma * epsilon;
 
-console.log(powerConsumption);
+// console.log(powerConsumption);
+// ------------
+
+
+let oxygen = [...sum];
+let scrubber = [...sum];
+
+transposedSum.forEach((b, i, a) => {
+    // console.log(b)
+    const ones = b.filter(bit => bit === 1).length;
+    const zeros = b.filter(bit => bit === 0).length;
+
+    oxygen = oxygen.length > 1 ? oxygen.filter(s => s[i] === (ones >= zeros ? 1 : 0)) : oxygen;
+    scrubber = scrubber.length > 1 ? scrubber.filter(s => s[i] === (ones >= zeros ? 0 : 1)) : scrubber;
+
+    console.log(oxygen.length, scrubber.length);
+
+})
+
+
+
+
+// console.log(parseInt(oxygen[0].join(''), 2), parseInt(scrubber[0].join(''), 2));
+
+
+
+
+const life = parseInt(oxygen[0].join(''), 2) * parseInt(scrubber[0].join(''), 2);
+console.log(life)
+
