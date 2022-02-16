8efa249d3adad5c8e4d3c1d20923e4da034bf8e1
Tiago Magalhães <tiagomsmagalhaes@users.noreply.github.com>
2022-02-16 17:09:16 +0000

    day3 part2 success

--- day3/part2.js
+++ day3/part2.js
@@ -9,10 +9,6 @@ const sum = fs.readFileSync(`${__dirname}/input.txt`)
     maxDigits = b.length > maxDigits ? b.length : maxDigits;
     return b;
 })
-
-const oxygen = obtainRating(sum, true);
-const scrubber = obtainRating(sum, false);
-
 function obtainRating(sum, mostCommon) {
     let transposedSum = [];
 
@@ -24,16 +20,17 @@ function obtainRating(sum, mostCommon) {
         const ones = bitsAtIndex.filter(bit => bit === 1).length;
         const zeros = bitsAtIndex.filter(bit => bit === 0).length;
 
-        console.log(ones, zeros);
         if (mostCommon) {
             sum = sum.length > 1 ? sum.filter(s => s[index] === (ones >= zeros ? 1 : 0)) : sum;
         } else {
             sum = sum.length > 1 ? sum.filter(s => s[index] === (ones >= zeros ? 0 : 1)) : sum;
         }
-
     }
     return parseInt(sum[0].join(''), 2);
 }
+
+const oxygen = obtainRating(sum, true);
+const scrubber = obtainRating(sum, false);
 
 const life = oxygen * scrubber;
 console.log(life);
